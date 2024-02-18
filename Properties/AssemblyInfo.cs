7bb24d5cd3cc790f7a675ed0a936b5002f1c5f47
kortea01 <136923555+kortea01@users.noreply.github.com>
2024-02-18 12:26:32 +0100

    Add files via upload

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestDolgozatProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestDolgozatProject")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0bd97da2-fd2e-4e2d-a46d-eaf88f55b643")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TestDolgozatProject
+{
+
+    private Array pontok = new Array[];
+
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void PontFelvesz(int x)
+        {
+            //TODO: Hozzáad egy pontszámot a listához.Érvénytelen paraméter  esetén  (x  < -1  vagy  x  >  100)  ez  a  függvény  dobjon ArgumentException-t.
+        }
+        public bool MindenkiMegirta()
+        {
+            //TODO: Volt-e olyan diák, aki nem írt dolgozatot.
+            return true;
+        }
+
+        public int Bukas()
+        {
+            //TODO: Adja  vissza hányan értek el 50 pontnál kevesebbet!C#  gettert használj!Figyeljen oda, hogy csak azokat a tanulókat számolja, akik megírták a dolgozatot.
+            return 0;
+        }
+
+        public int Elegseges()
+        {
+            //TODO: Adja vissza hányan értek el [50,60] pont közötti értéket.A szélsőértékeket is számolja bele! C# gettert használj!
+            return 0;
+        }
+
+        public int Kozepes()
+        {
+            //TODO: Adja vissza hányan értek el [61,70] pont közötti értéket.A szélsőértékeket is számolja bele! C# gettert használj!
+            return 0;
+        }
+
+        public int Jo()
+        {
+            //TODO: Adja vissza hányan értek el [71,80] pont közötti értéket.A szélsőértékeket is számolja bele! C# gettert használj!
+            return 0;
+        }
+
+        public int Jeles()
+        {
+            //TODO: Adja vissza hányan értek el 81 pontnál többet. C# gettert használj.
+            return 0;
+        }
+
+        public bool Gyanus(int kivalok)
+        {
+            //TODO: Adja vissza, hogya kivalo tanulok számánál lett-e több jeles osztályzat (db).
+            //(Pl.: kivalok=5 esetén6 db jeles osztályzat született, akkor a  visszatérési  érték  IGAZ  lesz!
+            //Lehetséges,  hogy  valamelyiktanuló  extra segítséget használt a dolgozat írása közben.).
+            //A kivalok számát 0-tól kezdődően várja  a  függvény.
+            //Érvénytelen  paraméter  esetén  ez  a  függvény  is  dobjon ArgumentException-t.
+            return true;
+        }
+
+        public bool Ervenytelen()
+        {
+            //TODO: Adja vissza, hogy a dolgozat érvénytelen-e, és újra kell írni. A   dolgozat   akkorlesz  érvénytelen,
+            //ha  a  tanulók  legalább  fele  nem  írt dolgozatot. C# gettert használj.
+            return true;
+        }
+
+
+
+    }
+}
